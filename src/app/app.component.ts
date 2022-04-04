fa76914a27d5bd6edc7dae4ce23e6566ebc82363
abhishekonlive <abhishekonlive@gmail.com>
2022-04-04 16:14:39 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component, VERSION } from '@angular/core';
 
+let age: Number;
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  //primitive type: string, number, boolean
+  //Complex type: array, object
+  //Function Type
+  age = 'asfsd';
   name = 'Angular ' + VERSION.major;
 }
